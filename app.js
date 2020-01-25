379c0235b81d7c82f253d3ac32e66aa7b0501d16
Laura Schulz <lrs8810@gmail.com>
2020-01-25 15:14:10 -0700

    Add endpoint post footnotes

--- app.js
+++ app.js
@@ -58,3 +58,23 @@ app.post('/api/v1/papers', (request, response) => {
       response.status(500).json({ error });
     });
 });
+
+app.post('/api/v1/footnotes', (request, response) => {
+  const footnote = request.body;
+
+  for (let requiredParameter of ['note', 'paper_id']) {
+    if (!footnote[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { note: <String>, paper_id: <Integer> }. You're missing a "${requiredParameter}" property.`});
+    }
+  }
+
+  database('footnotes').insert(footnote, 'id')
+    .then(footnote => {
+      response.status(201).json({ id: footnote[0]})
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
